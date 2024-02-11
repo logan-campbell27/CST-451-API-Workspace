db8eefd86e5f89030f23fcd129b5e6095bfb4b2d
Logan Campbell <logan.cam272003@gmail.com>
2024-02-11 15:52:42 -0700

    Added all teams

--- football-assistant-frontend/src/components/PlayerTable.vue
+++ football-assistant-frontend/src/components/PlayerTable.vue
@@ -12,10 +12,41 @@
                 <option value="TE">TE</option>
             </select>
             <select v-model="team">
-                <option value="">Team</option>
-                <option value="CLE">CLE</option>
-                <option value="DET">DET</option>
-            </select>
+    <option value="">Team</option>
+    <option value="ARI">ARI</option>
+    <option value="ATL">ATL</option>
+    <option value="BAL">BAL</option>
+    <option value="BUF">BUF</option>
+    <option value="CAR">CAR</option>
+    <option value="CHI">CHI</option>
+    <option value="CIN">CIN</option>
+    <option value="CLE">CLE</option>
+    <option value="DAL">DAL</option>
+    <option value="DEN">DEN</option>
+    <option value="DET">DET</option>
+    <option value="GB">GB</option>
+    <option value="HOU">HOU</option>
+    <option value="IND">IND</option>
+    <option value="JAX">JAX</option>
+    <option value="KC">KC</option>
+    <option value="LAC">LAC</option>
+    <option value="LAR">LAR</option>
+    <option value="LV">LV</option>
+    <option value="MIA">MIA</option>
+    <option value="MIN">MIN</option>
+    <option value="NE">NE</option>
+    <option value="NO">NO</option>
+    <option value="NYG">NYG</option>
+    <option value="NYJ">NYJ</option>
+    <option value="PHI">PHI</option>
+    <option value="PIT">PIT</option>
+    <option value="SF">SF</option>
+    <option value="SEA">SEA</option>
+    <option value="TB">TB</option>
+    <option value="TEN">TEN</option>
+    <option value="WAS">WAS</option>
+</select>
+
             <table id="players">
       <tr>
         <th>Name</th>
